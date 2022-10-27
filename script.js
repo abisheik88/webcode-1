99423afe885890c9fc74caab316a4192360e1900
Abisheik <77277634+abisheik88@users.noreply.github.com>
2022-10-27 07:48:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+let currentPage = 1;
+let per = 20;
+let sear;
+
+var header = document.createElement("div");
+header.setAttribute("class", "header");
+header.innerHTML = "<p><b>Open Brewery DB<b></p>";
+header.style.fontSize="30px"
+var sidenav = document.createElement("div");
+sidenav.setAttribute("class", "sidenav");
+sidenav.innerHTML = "Welcome<br> To <br>Brewery DB"
+sidenav.style.fontSize="30px"
+
+var footer = document.createElement("div");
+footer.setAttribute("class", "footer");
+footer.innerHTML = `
+<p id="currentPage"></p>
+<button type="button" onclick="previous()">Previous</button>
+<button onclick="next()">Next</button><button type="button" onclick="previous()>NEXT</button>`
+var section = document.createElement("section");
+section.setAttribute("class", "section")
+document.body.append(header, sidenav, footer, section);
+
+
+let searchE = document.createElement("div");
+searchE.setAttribute("class","form-row");
+let formrow = document.createElement("div");
+formrow.setAttribute("class","col");
+let  input= document.createElement("input");
+input.setAttribute("type","text");
+input.setAttribute("class","form-control");
+let butt=document.createElement("div");
+butt.setAttribute("type","button");
+butt.setAttribute("class","btn btn-secondary btn-lg");
+butt.innerHTML="Search";
+butt.style.marginLeft="10px"
+butt.style.width="95px"
+butt.style.backgroundColor="black"
+
+butt.addEventListener("click",func)
+
+async function func(){
+
+
+}
+
+formrow.append(input);
+
+searchE.append(formrow,butt);
+header.append(searchE);
+
+
+function createData({ name, brewery_type, street, address_2, address_3, city, state, postal_code, country, phone, website_url }) {
+    var container = document.createElement("div");
+    container.setAttribute("class", "container");
+    container.innerHTML = `
+<p>Name: ${name}</p>
+<p>type:${brewery_type}</p>
+<p>Street: ${street}</p>
+<p>Address: ${address_2, address_3}</p>
+<p>City: ${city}</p>
+<p>State: ${state}</p>
+<p>Postal code: ${postal_code}</p>
+<p>Country: ${country}<p>
+<p>Phone: ${phone}</p>
+<p>Website: ${website_url}</P>`
+    section.appendChild(container);
+}
+
+async function per_page(num) {
+    document.querySelector(".box").innerHTML = ``;
+    try {
+        let data = await fetch(`https://api.openbrewerydb.org/breweries?per_page=${num}`);
+        let info = await data.json();
+        console.log(info);
+        info.forEach((brewery) => createData(brewery));
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+}
+
+page(1);
+
+async function page(num) {
+    document.querySelector(".section").innerHTML = ``;
+    document.querySelector("#currentPage").innerHTML = `Page: ${num}`
+
+
+    try {
+        let data = await fetch(`https://api.openbrewerydb.org/breweries?per_page=${per}&page=${num}`);
+        let info = await data.json();
+        console.log(info);
+        info.forEach((brewery) => createData(brewery));
+    }
+    catch (error) {
+        console.error(error);
+    }
+    currentPage = num;
+}
+
+
+function previous() {
+    let a = currentPage - 1;
+    if (a < 1) {
+        page(1);
+    }
+    else {
+        page(a);
+    }
+}
+
+function next() {
+    let b = currentPage + 1;
+    page(b);
+}
+
